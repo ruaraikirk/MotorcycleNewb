8e0cceb7d4b084a9c9346254f6b166bb44b6fee1
Ruarai Kirk <ruaraikirk@gmail.com>
2019-05-14 17:51:39 +0100

    Add project files.

---
+++ MotorcycleNewb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MotorcycleNewb.Startup))]
+namespace MotorcycleNewb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
